6b9e246ec11ea029c46242a61d57791c042be0a4
Giuseppe Diniz <GDinizperes@gmail.com>
2024-01-31 15:07:34 -0300

    feat(view-list): implementação da lista de arrays no localStorage

--- src/app/data-form/data-form.component.ts
+++ src/app/data-form/data-form.component.ts
@@ -1,3 +1,4 @@
+// data-form.component.ts
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
@@ -6,16 +7,16 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   templateUrl: './data-form.component.html',
   styleUrls: ['./data-form.component.css']
 })
-
 export class DataFormComponent implements OnInit {
   formulario!: FormGroup;
+  nextId: number = 1; // Inicialize o contador com 1 ou qualquer valor inicial desejado
 
   constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
     // Inicialize o formulário e defina as validações necessárias
     this.formulario = this.fb.group({
-      id: [''], // Você pode adicionar um valor padrão aqui se necessário
+      id: [this.getNextId(), Validators.required], // Use o método para obter o próximo ID
       nome: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
       cep: [''],
@@ -37,5 +38,12 @@ export class DataFormComponent implements OnInit {
 
     // Armazene no sessionStorage
     sessionStorage.setItem('seuChaveSessionStorage', JSON.stringify(formData));
+
+    // Atualize o ID para o próximo valor
+    this.nextId++;
+  }
+
+  private getNextId(): number {
+    return this.nextId;
   }
 }
--- src/app/view-list/view-list.component.ts
+++ src/app/view-list/view-list.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-view-list',
   templateUrl: './view-list.component.html',
-  styleUrl: './view-list.component.css'
+  styleUrls: ['./view-list.component.css']
 })
-export class ViewListComponent {
+export class ViewListComponent implements OnInit {
+  localStorageData: any[] = [];
+  sessionStorageData: any[] = [];
 
+  constructor() { }
+
+  ngOnInit(): void {
+    // Obtenha os dados do localStorage
+    const localStorageString = localStorage.getItem('seuChaveLocalStorage');
+    this.localStorageData = localStorageString ? [JSON.parse(localStorageString)] : [];
+
+    // Obtenha os dados do sessionStorage
+    const sessionStorageString = sessionStorage.getItem('seuChaveSessionStorage');
+    this.sessionStorageData = sessionStorageString ? [JSON.parse(sessionStorageString)] : [];
+  }
 }
